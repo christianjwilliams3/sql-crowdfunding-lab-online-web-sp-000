0f856d7334377d0ee1200c912a69c1507fda32d0
christian williams <christian.williamsj@gmail.com>
2019-11-20 00:43:17 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -19,7 +19,4 @@ INSERT INTO users (name) VALUES ("Mel");
 INSERT INTO users (name) VALUES ("Tony");
 INSERT INTO users (name) VALUES ("Kimo");
 
-INSERT INTO cats (name, age, breed) VALUES ("Maru", 3, "Scottish Fold");
-INSERT INTO cats (name, age, breed) VALUES ("Hana", 1, "Tabby");
-INSERT INTO cats (name, age, breed) VALUES ("Nona", 4, "Tortoiseshell");
-INSERT INTO cats (name, age, breed) VALUES ("Lil' Bub", 2, "perma-kitten");
+
