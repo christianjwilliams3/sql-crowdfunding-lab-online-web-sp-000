22373b6923d329fd269f75df71173c5b74cbc607
christian williams <christian.williamsj@gmail.com>
2019-11-20 00:42:52 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO users (name) VALUES ("mugumogu");
+INSERT INTO users (name) VALUES ("Sophie");
+INSERT INTO users (name) VALUES ("Penny");
+INSERT INTO users (name) VALUES ("John");
+INSERT INTO users (name) VALUES ("Bill");
+INSERT INTO users (name) VALUES ("Lauren");
+INSERT INTO users (name) VALUES ("Christian");
+INSERT INTO users (name) VALUES ("Danny");
+INSERT INTO users (name) VALUES ("Liam");
+INSERT INTO users (name) VALUES ("Skeletor");
+INSERT INTO users (name) VALUES ("Quinn");
+INSERT INTO users (name) VALUES ("Louis");
+INSERT INTO users (name) VALUES ("Marlow");
+INSERT INTO users (name) VALUES ("Lama");
+INSERT INTO users (name) VALUES ("Bob");
+INSERT INTO users (name) VALUES ("Dick");
+INSERT INTO users (name) VALUES ("Chad");
+INSERT INTO users (name) VALUES ("Mel");
+INSERT INTO users (name) VALUES ("Tony");
+INSERT INTO users (name) VALUES ("Kimo");
+
+INSERT INTO cats (name, age, breed) VALUES ("Maru", 3, "Scottish Fold");
+INSERT INTO cats (name, age, breed) VALUES ("Hana", 1, "Tabby");
+INSERT INTO cats (name, age, breed) VALUES ("Nona", 4, "Tortoiseshell");
+INSERT INTO cats (name, age, breed) VALUES ("Lil' Bub", 2, "perma-kitten");
